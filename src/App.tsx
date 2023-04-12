453f5bc7dd95f2f5bd2b5650fe49eaf8fc456bb0
Iago <100394244+IagoAparecido@users.noreply.github.com>
2023-04-12 09:50:46 -0400

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub Buscador</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import { BsSearch } from "react-icons/bs";
+
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+function Search({ loadUser }: SearchProps) {
+  const [userName, setUserName] = useState("");
+
+  return (
+    <div>
+      <h2>Busque por um usuário:</h2>
+      <p>Conheça seus melhores repositórios</p>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
